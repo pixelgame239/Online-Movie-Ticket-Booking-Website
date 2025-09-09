1ce6f79ee40d6dadf6d393fa6e205f0958252308
namsss098s <91941620+namsss098s@users.noreply.github.com>
2025-09-09 08:47:44 +0000

    views

--- moviebackend/movies/views.py
+++ moviebackend/movies/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import Movie, Cinema, Showtime
+from .forms import MovieForm, ShowtimeForm
 
-# Create your views here.
+def admin_required(user):
+    return user.is_authenticated and user.is_admin
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    query = request.GET.get('q')
+    genre = request.GET.get('genre')
+    if query:
+        movies = movies.filter(title__icontains=query)
+    if genre:
+        movies = movies.filter(genre__icontains=genre)
+    return render(request, 'movies/movie_list.html', {'movies': movies})
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    showtimes = movie.showtimes.all()
+    return render(request, 'movies/movie_detail.html', {'movie': movie, 'showtimes': showtimes})
+
+@login_required
+@user_passes_test(admin_required)
+def movie_create(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('movie_list')
+    else:
+        form = MovieForm()
+    return render(request, 'movies/movie_form.html', {'form': form})
+
+@login_required
+@user_passes_test(admin_required)
+def movie_update(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES, instance=movie)
+        if form.is_valid():
+            form.save()
+            return redirect('movie_detail', pk=movie.pk)
+    else:
+        form = MovieForm(instance=movie)
+    return render(request, 'movies/movie_form.html', {'form': form})
+
+@login_required
+@user_passes_test(admin_required)
+def movie_delete(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    movie.delete()
+    return redirect('movie_list')
+
+@login_required
+@user_passes_test(admin_required)
+def showtime_create(request):
+    if request.method == 'POST':
+        form = ShowtimeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('movie_list')
+    else:
+        form = ShowtimeForm()
+    return render(request, 'movies/showtime_form.html', {'form': form})
--- moviebackend/users/models.py
+++ moviebackend/users/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
 
-class CustomerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=15,blank=True)
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    phone = models.CharField(max_length=15, blank=True)
     address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
