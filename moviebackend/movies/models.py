c2716911e422a6055c8957baedfeca74056caa65
pixelgame239 <pixelgame2004@outlook.com.vn>
2025-09-19 22:14:22 +0700

    models syncing

--- moviebackend/movies/models.py
+++ moviebackend/movies/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=50, primary_key=True)
+
+    def __str__(self):
+        return self.genre_name
+    
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    genre = models.CharField(max_length=50)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name="movies")
     duration = models.PositiveIntegerField(help_text="Duration in minutes")
     description = models.TextField(blank=True)
-    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
+    poster = models.URLField(max_length=300, blank=True, null=True)
     release_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
--- moviebackend/users/models.py
+++ moviebackend/users/models.py
@@ -6,6 +6,18 @@ class User(AbstractUser):
     is_admin = models.BooleanField(default=False)
     phone = models.CharField(max_length=15, blank=True)
     address = models.TextField(blank=True)
-
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=1,
+        choices=[('M', 'Nam'), ('F', 'Nữ')],
+        null=True,
+        blank=True
+    )
+    favorite_cinema = models.ForeignKey(
+        'movies.Cinema',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
     def __str__(self):
         return self.username
