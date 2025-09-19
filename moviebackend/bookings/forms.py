6a4539785efd15cab83a85ee9a911303d97db75a
namsss098s <91941620+namsss098s@users.noreply.github.com>
2025-09-19 11:16:56 +0000

    del

--- moviebackend/bookings/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Payment
-
-class PaymentForm(forms.ModelForm):
-    method = forms.ChoiceField(choices=Payment.PAYMENT_METHODS, widget=forms.RadioSelect)
-
-    class Meta:
-        model = Payment
-        fields = ['method']
