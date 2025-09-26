b810382d99180540ed6edb0e45afbcd4286f1f27
pixelgame239 <pixelgame2004@outlook.com.vn>
2025-09-26 17:30:10 +0700

    add environment

--- moviebackend/moviebackend/settings.py
+++ moviebackend/moviebackend/settings.py
@@ -4,9 +4,16 @@ Django settings for moviebackend project.
 
 from pathlib import Path
 import os
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # --- BASE DIR ---
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # --- SECURITY ---
 SECRET_KEY = 'django-insecure-3f31*p3*f=_il7d(t&25-9t4qfy21o5_)31&fvhq**7lr4o1-d'
@@ -75,12 +82,12 @@ WSGI_APPLICATION = 'moviebackend.wsgi.application'
 # --- DATABASE ---
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER':'postgres.anxkatxbafgpahipxzsw',
-        'PASSWORD':"group2database",
-        'HOST': "aws-1-ap-southeast-1.pooler.supabase.com",
-        'PORT': '6543',
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
@@ -111,11 +118,11 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 #Email
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Use SMTP backend for email sending
-EMAIL_HOST = 'smtp.gmail.com'  # Gmail SMTP server
-EMAIL_PORT = 587  # Gmail uses port 587 for TLS encryption
-EMAIL_USE_TLS = True  # Enable TLS encryption
-EMAIL_HOST_USER = '20221580@eaut.edu.vn'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'xaxr hdgb ajsd dkvk'  # Your app password (if using Gmail)
-DEFAULT_FROM_EMAIL = '20221580@eaut.edu.vn'  # The default sender address
