68d2c3515548344f3758da8550ca1358815cb4b3
pixelgame239 <pixelgame2004@outlook.com.vn>
2025-09-28 22:36:30 +0700

    settings.py reset

--- moviebackend/moviebackend/settings.py
+++ moviebackend/moviebackend/settings.py
@@ -17,7 +17,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # --- SECURITY ---
 SECRET_KEY = 'django-insecure-3f31*p3*f=_il7d(t&25-9t4qfy21o5_)31&fvhq**7lr4o1-d'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["online-movie-ticket-booking-website.onrender.com", 'localhost', '127.0.0.1', "online-movie-ticket-booking-website-production.up.railway.app", "https://anmoviesweb.pythonanywhere.com","*"]
 CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_HTTPONLY = True
